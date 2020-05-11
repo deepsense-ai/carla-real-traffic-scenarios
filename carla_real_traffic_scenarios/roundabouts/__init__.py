d2f5f49d00edae7ca661396db292490c14781ff4
Michał Martyniak <michal.martyniak@deepsense.ai>
2020-05-11 16:14:40 +0200

    Add collision sensor

--- carla_real_traffic_scenarios/roundabouts/__init__.py
+++ carla_real_traffic_scenarios/roundabouts/__init__.py
@@ -37,7 +37,9 @@ class RoundaboutExitingScenario(Scenario):
         # Saving state between consecutive steps
         self._next_route_checkpoint_idx: Optional[int] = None
         self._steps_to_reach_next_checkpoint: Optional[int] = None
-        self._command: ChauffeurCommand = None
+        self._command: Optional[ChauffeurCommand] = None
+        self._collision_sensor: Optional[carla.Actor] = None
+        self._collided: bool = False
 
         # Driving actors
         vehicle_blueprints = self._world.get_blueprint_library().filter("vehicle.*")
@@ -66,15 +68,31 @@ class RoundaboutExitingScenario(Scenario):
         start_node = random.choice(TOWN03_ROUNDABOUT_NODES)
         start_node.spawn_point.location.z = 0.1
 
+        if self._collision_sensor is not None:
+            self._collision_sensor.destroy()
+            self._collision_sensor = None
+
+        blueprint_library = self._world.get_blueprint_library()
+        collision_sensor_bp = blueprint_library.find("sensor.other.collision")
+        self._collision_sensor = self._world.spawn_actor(
+            collision_sensor_bp, ego_vehicle.get_transform(), attach_to=ego_vehicle
+        )
+
+        def on_collision(e):
+            self._collided = True
+
+        self._collision_sensor.listen(on_collision)
+        self._collided = False
+
         # Physics trick is necessary to prevent vehicle from keeping the velocity
         ego_vehicle.set_simulate_physics(False)
         ego_vehicle.set_transform(start_node.spawn_point)
         ego_vehicle.set_simulate_physics(True)
 
         # Route
-        self._take_nth_exit = random.randrange(1, 5)
+        take_nth_exit = random.randrange(1, 5)
         self._route = route.build_roundabout_checkpoint_route(
-            start_node=start_node, nth_exit_to_take=self._take_nth_exit
+            start_node=start_node, nth_exit_to_take=take_nth_exit
         )
 
         # States
@@ -101,6 +119,10 @@ class RoundaboutExitingScenario(Scenario):
         reward = 0
         done = False
         info = {}
+
+        if self._collided is True:
+            reward = 0
+            done = True
 
         if ego_location in checkpoint_area:
             if not self._sparse_reward_mode:

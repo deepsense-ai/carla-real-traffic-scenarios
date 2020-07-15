599fc810759ea9e3ac915d5e98c14de794c1ac50
Pawel Ziecina <1385347+pawel-ziecina@users.noreply.github.com>
2020-07-15 10:49:59 +0200

    Update rewards in NGSIM scenario

--- carla_real_traffic_scenarios/early_stop.py
+++ carla_real_traffic_scenarios/early_stop.py
@@ -2,24 +2,11 @@ import logging
 from typing import Optional
 
 import carla
-from enum import IntFlag, auto, _decompose
 
 from carla_real_traffic_scenarios.trajectory import Trajectory
 from carla_real_traffic_scenarios.utils.carla import CollisionSensor
 
 LOGGER = logging.getLogger(__name__)
-
-
-class EarlyStop(IntFlag):
-    NONE = 0
-    MOVED_TOO_FAR = auto()
-    TIMEOUT = auto()
-    OFFROAD = auto()
-    COLLISION = auto()
-    ERROR = auto()
-
-    def decomposed_name(self, sep='|'):
-        return sep.join([e.name for e in _decompose(self.__class__, self)[0]])
 
 
 class EarlyStopMonitor:
@@ -40,45 +27,39 @@ class EarlyStopMonitor:
         return snapshot.timestamp.elapsed_seconds
 
     def __call__(self, ego_transform: carla.Transform):
-        early_stop = EarlyStop.NONE
+        early_stop = False
         for check_fn in [self._check_collision, self._check_offroad, self._check_timeout, self._check_move_away]:
             early_stop |= check_fn(ego_transform)
+            if early_stop:
+                break
 
         return early_stop
 
     def _check_move_away(self, ego_transform):
-        move_away = EarlyStop.NONE
+        move_away = False
         if self._trajectory:
             *_, distance_m = self._trajectory.find_nearest_trajectory_point(ego_transform)
-            if distance_m > self._max_trajectory_distance_m:
-                move_away = EarlyStop.MOVED_TOO_FAR
+            move_away = distance_m > self._max_trajectory_distance_m  # moved_away
+            if move_away:
                 LOGGER.debug(f'Vehicle moved too far: {distance_m:0.3f}m/{self._max_trajectory_distance_m:0.3f}m')
         return move_away
 
     def _check_timeout(self, *_):
-        timeout = EarlyStop.NONE
         elapsed_s = self._get_timestamp_s(self._world) - self._start_timestamp_s
-        if self._timeout_s is not None and elapsed_s > self._timeout_s:  # timeout
-            timeout = EarlyStop.TIMEOUT
+        timeout = self._timeout_s is not None and elapsed_s > self._timeout_s  # timeout
+        if timeout:
             LOGGER.debug(f'Timeout elapsed: {elapsed_s:0.3f}s while timeout is set to {self._timeout_s:0.3f}s')
         return timeout
 
     def _check_offroad(self, ego_transform):
-        offroad = EarlyStop.NONE
-        if self._world_map.get_waypoint(ego_transform.location, project_to_road=False) is None:
-            waypoint = self._world_map.get_waypoint(ego_transform.location)
-            distance_to_lane = waypoint.transform.location.distance(ego_transform.location)
-            # if distance to nearest lane ceneter not exceeds 30% of half of lane width
-            probably_is_on_lane = (waypoint.lane_width / 2) * 1.3 > distance_to_lane
-            if not probably_is_on_lane:
-                offroad = EarlyStop.OFFROAD
-                LOGGER.debug(f'Vehicle off-road')
+        offroad = self._world_map.get_waypoint(ego_transform.location, project_to_road=False) is None
+        if offroad:
+            LOGGER.debug(f'Vehicle off-road')
         return offroad
 
     def _check_collision(self, *_):
-        collided = EarlyStop.NONE
-        if self._collision_sensor.has_collided:
-            collided = EarlyStop.COLLISION
+        collided = self._collision_sensor.has_collided
+        if collided:
             LOGGER.debug(f'Vehicle collision')
         return collided
 
--- carla_real_traffic_scenarios/trajectory.py
+++ carla_real_traffic_scenarios/trajectory.py
@@ -1,19 +1,13 @@
-import logging
 from typing import List, Tuple
 
 import carla
 import more_itertools
 import numpy as np
 import scipy.spatial
-
-from carla_real_traffic_scenarios.scenario import ChauffeurCommand
-from carla_real_traffic_scenarios.utils.geometry import normalize_angle
-from carla_real_traffic_scenarios.utils.topology import get_lane_id
-from carla_real_traffic_scenarios.utils.transforms import distance_between_on_plane
-
-LOGGER = logging.getLogger(__name__)
+from functools import lru_cache
 
 
+@lru_cache()
 def _get_nearest_location(locations_carla: np.ndarray, location_ego: np.ndarray):
     dm = scipy.spatial.distance_matrix([location_ego], locations_carla)
     idx = int(np.argmin(dm, axis=1)[0])
@@ -29,115 +23,19 @@ class Trajectory:
         self._segments_length_m = [
             t1.location.distance(t2.location) for t1, t2 in more_itertools.windowed(trajectory_carla, 2)
         ]
-
-        self._s = np.cumsum(np.pad(self._segments_length_m, 1, mode='constant'))
-        last_idx = len(trajectory_carla) - 1
-        self._length_m = self._s[last_idx]
+        self._length_m = sum(self._segments_length_m)
 
     def find_nearest_trajectory_point(self, transform_carla: carla.Transform) -> Tuple[int, carla.Transform, float]:
-        location_ego = np.array([transform_carla.location.x, transform_carla.location.y])
+        location_ego = np.round([transform_carla.location.x, transform_carla.location.y], decimals=1)
         idx, distance_from_trajectory = _get_nearest_location(self._locations_carla, location_ego)
+        return idx, self._trajectory_carla[idx], distance_from_trajectory
 
-        return self._s[idx], self._trajectory_carla[idx], distance_from_trajectory
+    def get_distance_from_start(self, transform_carla: carla.Transform):
+        idx, *_, distance_from_trajectory = self.find_nearest_trajectory_point(transform_carla)
 
     @property
     def total_length_m(self):
         return self._length_m
 
-
-CROSSTRACK_ERROR_TOLERANCE = 0.3
-YAW_DEG_ERRORS_TOLERANCE = 10
-TARGET_LANE_ALIGNMENT_FRAMES = 10
-
-
-class LaneAlignmentMonitor:
-
-    def __init__(self, lane_alignment_frames, cross_track_error_tolerance, yaw_deg_error_tolerance):
-        self._cross_track_error_tolerance = cross_track_error_tolerance
-        self._yaw_rad_error_tolerance = np.deg2rad(yaw_deg_error_tolerance)
-        self._lane_alignment_frames = lane_alignment_frames
-        self._lane_alignment_counter = 0
-        self._last_cross_track_error = 0
-        self._last_yaw_rad_error = 0
-
-    def reset(self):
-        self._lane_alignment_counter = 0
-        self._last_cross_track_error = 0
-        self._last_yaw_rad_error = 0
-
-    def is_lane_aligned(self, ego_transform: carla.Transform, lane_transform: carla.Transform):
-        cross_track_error = distance_between_on_plane(ego_transform.location, lane_transform.location)
-        yaw_rad_error = normalize_angle(np.deg2rad(ego_transform.rotation.yaw - lane_transform.rotation.yaw))
-        aligned_with_target_lane = cross_track_error < self._cross_track_error_tolerance and \
-                                   np.abs(yaw_rad_error) < self._yaw_rad_error_tolerance
-        if aligned_with_target_lane:
-            self._lane_alignment_counter += 1
-        else:
-            self._lane_alignment_counter = 0
-        self._last_cross_track_error = cross_track_error
-        self._last_yaw_rad_error = yaw_rad_error
-        return self._lane_alignment_counter == self._lane_alignment_frames
-
-    def info(self):
-        alignment_errors = str(f'track={self._last_cross_track_error:0.2f}m yaw={self._last_yaw_rad_error:0.2f}rad')
-        return {
-            'alignment_errors': alignment_errors,
-            'target_alignment_counter': self._lane_alignment_counter
-        }
-
-
-class LaneChangeProgressMonitor:
-
-    def __init__(self, world_map: carla.Map, *,
-                 start_lane_ids: List[Tuple[int, int, int]], target_lane_ids: List[Tuple[int, int, int]],
-                 lane_change_command: ChauffeurCommand, checkpoints_number: int = 10):
-        self._world_map = world_map
-        self._start_lane_ids = start_lane_ids
-        self._target_lane_ids = target_lane_ids
-        self._lane_change_command = lane_change_command
-        self._checkpoints_number = checkpoints_number
-
-        self._total_distance_m = None
-        self._checkpoints_distance_m = None
-        self._previous_progress = 0
-
-    def get_progress_change(self, ego_transform: carla.Transform):
-        current_location = ego_transform.location
-        current_waypoint = self._world_map.get_waypoint(current_location)
-        lane_id = get_lane_id(current_waypoint)
-        on_start_lane = lane_id in self._start_lane_ids
-        on_target_lane = lane_id in self._target_lane_ids
-
-        # on init calculate "lane width"
-        if self._total_distance_m is None:
-            target_waypoint = self._get_target_lane_waypoint(current_waypoint)
-            self._total_distance_m = current_location.distance(target_waypoint.transform.location)
-            self._checkpoints_distance_m = self._total_distance_m / self._checkpoints_number
-
-        progress_change = 0
-        if on_start_lane:
-            target_waypoint = self._get_target_lane_waypoint(current_waypoint)
-            if target_waypoint:
-                progress_change = self._calc_progress_change(target_waypoint, current_location)
-            else:
-                LOGGER.info('Have no lane to perform maneuver')
-        elif on_target_lane:
-            target_waypoint = current_waypoint
-            progress_change = self._calc_progress_change(target_waypoint, current_location)
-
-        return progress_change
-
-    def _get_target_lane_waypoint(self, current_waypoint):
-        return {
-            ChauffeurCommand.CHANGE_LANE_LEFT: current_waypoint.get_left_lane,
-            ChauffeurCommand.CHANGE_LANE_RIGHT: current_waypoint.get_right_lane,
-        }[self._lane_change_command]()
-
-    def _calc_progress_change(self, target_waypoint, current_location):
-        distance_from_target = current_location.distance(target_waypoint.transform.location)
-        distance_traveled_m = self._total_distance_m - distance_from_target
-        checkpoints_passed_number = int(distance_traveled_m / self._checkpoints_distance_m)
-        progress = checkpoints_passed_number / self._checkpoints_number
-        progress_change = progress - self._previous_progress
-        self._previous_progress = progress
-        return progress_change
+    def __call__(self, transform_carla: carla.Transform) -> Tuple[bool, float]:
+        pass

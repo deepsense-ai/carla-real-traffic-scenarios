89e3a8cd86cc8dae758b629f9881aa260eada1d5
Pawel Ziecina <1385347+pawel-ziecina@users.noreply.github.com>
2020-07-16 10:37:13 +0200

    Add cause of early stop

--- carla_real_traffic_scenarios/early_stop.py
+++ carla_real_traffic_scenarios/early_stop.py
@@ -2,11 +2,24 @@ import logging
 from typing import Optional
 
 import carla
+from enum import IntFlag, auto, _decompose
 
 from carla_real_traffic_scenarios.trajectory import Trajectory
 from carla_real_traffic_scenarios.utils.carla import CollisionSensor
 
 LOGGER = logging.getLogger(__name__)
+
+
+class EarlyStop(IntFlag):
+    NONE = 0
+    MOVED_TO_FAR = auto()
+    TIMEOUT = auto()
+    OFFROAD = auto()
+    COLLISION = auto()
+    ERROR = auto()
+
+    def decomposed_name(self, sep='|'):
+        return sep.join([e.name for e in _decompose(self.__class__, self)[0]])
 
 
 class EarlyStopMonitor:
@@ -27,39 +40,41 @@ class EarlyStopMonitor:
         return snapshot.timestamp.elapsed_seconds
 
     def __call__(self, ego_transform: carla.Transform):
-        early_stop = False
+        early_stop = EarlyStop.NONE
         for check_fn in [self._check_collision, self._check_offroad, self._check_timeout, self._check_move_away]:
             early_stop |= check_fn(ego_transform)
-            if early_stop:
-                break
 
         return early_stop
 
     def _check_move_away(self, ego_transform):
-        move_away = False
+        move_away = EarlyStop.NONE
         if self._trajectory:
             *_, distance_m = self._trajectory.find_nearest_trajectory_point(ego_transform)
             move_away = distance_m > self._max_trajectory_distance_m  # moved_away
             if move_away:
+                move_away |= EarlyStop.MOVED_TO_FAR
                 LOGGER.debug(f'Vehicle moved too far: {distance_m:0.3f}m/{self._max_trajectory_distance_m:0.3f}m')
         return move_away
 
     def _check_timeout(self, *_):
+        timeout = EarlyStop.NONE
         elapsed_s = self._get_timestamp_s(self._world) - self._start_timestamp_s
-        timeout = self._timeout_s is not None and elapsed_s > self._timeout_s  # timeout
-        if timeout:
+        if self._timeout_s is not None and elapsed_s > self._timeout_s:  # timeout
+            timeout |= EarlyStop.TIMEOUT
             LOGGER.debug(f'Timeout elapsed: {elapsed_s:0.3f}s while timeout is set to {self._timeout_s:0.3f}s')
         return timeout
 
     def _check_offroad(self, ego_transform):
-        offroad = self._world_map.get_waypoint(ego_transform.location, project_to_road=False) is None
-        if offroad:
+        offroad = EarlyStop.NONE
+        if self._world_map.get_waypoint(ego_transform.location, project_to_road=False) is None:
+            offroad |= EarlyStop.OFFROAD
             LOGGER.debug(f'Vehicle off-road')
         return offroad
 
     def _check_collision(self, *_):
-        collided = self._collision_sensor.has_collided
-        if collided:
+        collided = EarlyStop.NONE
+        if self._collision_sensor.has_collided:
+            collided = EarlyStop.COLLISION
             LOGGER.debug(f'Vehicle collision')
         return collided
 
